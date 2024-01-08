8c42a36b34a86ed8565fa0432981ac2097fb67c2
Samie-ub <xxxsamie39@gmail.com>
2024-01-08 21:10:51 +0500

    first commit

---
+++ script/app.js
@@ -0,0 +1,9 @@
+const myAtropos = Atropos({
+    el: '.my-atropos',
+    distance: 10,
+        duration: 1,
+        easing: 'ease-out',
+        scale: 1,
+        rotate: { x: 1, y: 0, z: 0 },
+        shadow:false
+  });
